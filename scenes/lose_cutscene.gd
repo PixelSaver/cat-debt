9e03f7a1555ab1ad7d479cc49f6ae99a8d80f020
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2026-06-21 11:30:36 -0700

    add 2 more vine booms

--- scenes/lose_cutscene.gd
+++ scenes/lose_cutscene.gd
@@ -2,10 +2,17 @@ extends PixelMenu
 class_name LoseCutscene
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sad_text: RichTextLabel = $SadText
+@onready var audio_player1: AudioStreamPlayer = $AudioStreamPlayer
+@onready var audio_player2: AudioStreamPlayer = $AudioStreamPlayer2
+
+@export var sad_meow: AudioStreamMP3
+@export var vine_boom: AudioStreamMP3
 var ts : Array[Tweenable]
 var t : Tween
 
 func _ready():
+	sad_text.visible = false
 	start_anim()
 
 func start_anim():
@@ -15,9 +22,48 @@ func start_anim():
 
 func _anim_pain():
 	anim.play("cut_scene")
+	audio_player1.stream = sad_meow
+	sfx_tween_in(1, 0.05)
 	await anim.animation_finished
 	if t and t.is_running(): t.kill()
 	t = default_tween()
 	t.tween_property(anim, "scale", Vector2.ONE * 0.4, 2)
 	t.tween_property(anim, "position", Vector2(812, 350), 2)
+	anim.play("cat_down2")
+	#vine_boom
+	audio_player2.stream = vine_boom
+	await get_tree().create_timer(1).timeout
+	audio_player2.volume_db = 0
+	audio_player2.play()
+	
+
+func sfx_tween_in(ap: int, duration: float):
+	var sfx_t: Tween = create_tween()
+	sfx_t.set_ease(Tween.EASE_OUT)
+	sfx_t.set_trans(Tween.TRANS_QUINT).set_parallel(true)
+	
+	if ap == 1:
+		audio_player1.volume_db = -50
+		audio_player1.play()
+		sfx_t.tween_property(audio_player1, "volume_db", 0, duration)
+	elif ap == 2:
+		audio_player2.volume_db = -50
+		audio_player2.play()
+		sfx_t.tween_property(audio_player2, "volume_db", 0, duration)
+	
+func sfx_tween_out(ap: int, duration: float):
+	var sfx_t: Tween = create_tween()
+	sfx_t.set_ease(Tween.EASE_OUT)
+	sfx_t.set_trans(Tween.TRANS_QUINT).set_parallel(true)
+	
+	if ap == 1:
+		audio_player1.volume_db = 0
+		sfx_t.tween_property(audio_player1, "volume_db", -50, duration)
+		await sfx_t.finished
+		audio_player1.stop()
+	elif ap == 2:
+		audio_player2.volume_db = 0
+		sfx_t.tween_property(audio_player2, "volume_db", -50, duration)
+		await sfx_t.finished
+		audio_player2.stop()
 	
