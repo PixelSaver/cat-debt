9e7a6cd769c0b7d87928e688e7f6921decf700b5
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2026-06-21 02:41:47 -0700

    Working popup for selectoin

--- scenes/selection_popup.gd
+++ scenes/selection_popup.gd
@@ -1,9 +1,21 @@
-extends Panel
 class_name SelectionPopup
+extends Panel
 
-func _unhandled_input(event:InputEvent):
+@export var but: DefaultButton
+
+func _ready() -> void:
+	but.pressed.connect(_on_upgrade_pressed)
+
+func _unhandled_input(event: InputEvent):
 	if visible and event is InputEventMouseButton and event.pressed:
 		var rect = get_global_rect()
 		
 		if not rect.has_point(event.position):
 			hide()
+
+func _on_upgrade_pressed() -> void:
+	pass
+
+func _update_ui() -> void:
+	pass
+	# update level, price, and the availability of the button
